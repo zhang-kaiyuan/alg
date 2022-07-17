4395727c885d9922a1a0d1ed0d37c830bcbb3877
zhangkaiyuan <zhangky@cheche365.com>
2022-07-17 22:05:51 +0800

    feat: 工具删除方法

--- src/main/java/com/kaiy/common/MyArrayUtil.java
+++ src/main/java/com/kaiy/common/MyArrayUtil.java
@@ -1,9 +1,6 @@
 package com.kaiy.common;
 
 
-import java.util.Arrays;
-import java.util.function.Function;
-
 public class MyArrayUtil {
 
     public static void swap(int[] arr, int from, int to) {
@@ -16,21 +13,6 @@ public class MyArrayUtil {
         int[] dest = new int[src.length];
         System.arraycopy(src, 0, dest, 0, src.length);
         return dest;
-    }
-
-    public static void sortLogarithmic(Function<int[], int[]> f1) {
-        boolean flag = true;
-        for (int i = 0; i < 100000; i++) {
-            int[] sample = SamplesToGenerate.generateRandomArray(true);
-            int[] clone = MyArrayUtil.copy(sample);
-            Arrays.sort(sample);
-            f1.apply(clone);
-            if (!Arrays.equals(sample, clone)) {
-                flag = false;
-                break;
-            }
-        }
-        System.out.println(flag ? "nice!" : "shit!");
     }
 
 }
