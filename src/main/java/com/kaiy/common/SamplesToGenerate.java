d19150d98f605629961077e79fb0f2e3d1b71562
zhangkaiyuan <zhangky@cheche365.com>
2022-07-17 18:31:42 +0800

    feat: first commit

---
+++ src/main/java/com/kaiy/common/SamplesToGenerate.java
@@ -0,0 +1,28 @@
+package com.kaiy.common;
+
+import java.util.Arrays;
+
+public class SamplesToGenerate {
+
+    private static final int SAMPLE_SIZE = 100;
+    private static final int SAMPLE_RANGE = 100;
+
+    public static int[] generateRandomArray(boolean negative) {
+        int size = (int) (Math.random() * SAMPLE_SIZE);
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            int rand = (int) (Math.random() * SAMPLE_RANGE);
+            array[i] = whetherToGenerateNegative(negative) ? rand : rand * -1;
+        }
+        return array;
+    }
+
+    private static boolean whetherToGenerateNegative(boolean negative) {
+        return negative && ((int) (Math.random() * 2) & 1) == 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(generateRandomArray(true)));
+    }
+
+}
---
+++ src/main/java/com/kaiy/sort/SelectionSort.java
@@ -0,0 +1,5 @@
+package com.kaiy.sort;
+
+public class SelectionSort {
+
+}
